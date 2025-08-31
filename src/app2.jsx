fa648d13c438689cb09f62e8472f386571f4a9be
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2025-08-31 19:49:25 +0600

    Update app2.jsx

--- src/app2.jsx
+++ src/app2.jsx
@@ -1,1 +1,80 @@
+import Demo from "./component/Demo";
+import ContactForm from './component/ConatctForm';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Hero from './component/Hero';
+
+
+
+const App = () => {
+
+ let marks = 87;
+         const city = ["Dhaka","Chittagong","London","Dublin"];
+        const letter = ["e","q","a","f"];
+
+
+  return (
+    <div>
+      <Demo/>
+      <ContactForm/>
+      <Header/>
+      <Footer/>
+      <Hero/>
+      <button onClick = {() =>alert("Hello") }>Click</button>
+      <button style={
+        {
+          color : "red"
+        }
+      }>Style</button>
+    </div>
+    <div>
+        { marks>80 ? <h1>Passed</h1> : <h2>Failed</h2> }
+    </div>
+
+    <div>
+      {(
+        ()=>{
+          if(marks > 90){
+            return <h1>Result is : A+</h1>
+          }
+          else if(marks >=80 && marks <90){
+            return <h1>Result is : A</h1>
+          }
+          else{
+            return <h1>Failed</h1>
+          }
+        }
+      )()}
+    </div>
+
+    <div>
+      <ul>
+        {
+          city.map((cities,iterate)=>{
+            return <li key={iterate.toString()}>
+              {cities}
+            </li>
+          })
+        }
+      </ul>
+    </div>
+
+    <div>
+        <ol>
+          {
+            letter.map((alphabet,iterate)=>{
+              return <li key={iterate.toString()}>
+                {alphabet}
+              </li>
+            })
+          }
+        </ol>
+    </div>
+   
+  );
+
+
+
+}
+export default App;
 
