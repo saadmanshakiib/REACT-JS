ae8ba90e2ab7ab2cac32e5ec8811d8e892069aea
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2025-09-09 17:47:37 +0600

    Update app2.jsx

--- src/app2.jsx
+++ src/app2.jsx
@@ -5,94 +5,134 @@ import Footer from './component/Footer';
 import Hero from './component/Hero';
 
 
-const loginButton=(status)=>{
-  if(status){
-    return <button>Logout</button>
-  }
-  else{
-    return <button>Login</button>
-  }
-}
+// const loginButton=(status)=>{
+//   if(status){
+//     return <button>Logout</button>
+//   }
+//   else{
+//     return <button>Login</button>
+//   }
+// }
 
 
 const App = () => {
 
-  const status = false;
+  // const status = false; //automatically invoked function
+  // return(
+  //   <div>
+  //     {(()=>{
+  //       if(status == true){
+  //         return <button>Logout</button>
+  //       }
+  //       else{
+  //         return <button>Login</button>
+  //       }
+  //     })()}
+  //   </div>
+  // );
 
-  switch(status){
-    case true: return <button>Logout</button>
-    case false: return <button>Login</button>
-    default: return null;
-  }
+//  const status = false;
 
- let marks = 87;
-         const city = ["Dhaka","Chittagong","London","Dublin"];
-        const letter = ["e","q","a","f"];
+// let isLoggedIn = false;
+// return(
+
+//   <div>
+//     <h1>Login Status : </h1>
+//     {isLoggedIn && <button>LogOut</button>}
+//   </div>
+
+// );
+
+  // return(
+  
+  //    <div>
+  //      {
+  //       status? 
+  //        <button>Logout</button>
+  //       :
+  //       <button>Login</button>
+  //     }
+  //    </div>
+
+  // );
 
 
-  return(
-        // <div>
-        //   <h1>Login status</h1>
-        //   {loginButton(false)}
-        // </div>
+  // const status = false;
 
-      );
-    <div>
-      <Demo/>
-      <ContactForm/>
-      <Header/>
-      <Footer/>
-      <Hero/>
-      <button onClick = {() =>alert("Hello") }>Click</button>
-      <button style={
-        {
-          color : "red"
-        }
-      }>Style</button>
-    </div>
-    <div>
-        { marks>80 ? <h1>Passed</h1> : <h2>Failed</h2> }
-    </div>
+  // switch(status){
+  //   case true: return <button>Logout</button>
+  //   case false: return <button>Login</button>
+  //   default: return null;
+  // }
 
-    <div>
-      {(
-        ()=>{
-          if(marks > 90){
-            return <h1>Result is : A+</h1>
-          }
-          else if(marks >=80 && marks <90){
-            return <h1>Result is : A</h1>
-          }
-          else{
-            return <h1>Failed</h1>
-          }
-        }
-      )()}
-    </div>
+//  let marks = 87;
+         // const city = ["Dhaka","Chittagong","London","Dublin"];
+        // const letter = ["e","q","a","f"];
 
-    <div>
-      <ul>
-        {
-          city.map((cities,iterate)=>{
-            return <li key={iterate.toString()}>
-              {cities}
-            </li>
-          })
-        }
-      </ul>
-    </div>
 
-    <div>
-        <ol>
-          {
-            letter.map((alphabet,iterate)=>{
-              return <li key={iterate.toString()}>
-                {alphabet}
-              </li>
-            })
-          }
-        </ol>
-    </div>
+  // return(
+  //       // <div>
+  //       //   <h1>Login status</h1>
+  //       //   {loginButton(false)}
+  //       // </div>
+
+  //     );
+    // <div>
+    //   <Demo/>
+    //   <ContactForm/>
+    //   <Header/>
+    //   <Footer/>
+    //   <Hero/>
+    //   <button onClick = {() =>alert("Hello") }>Click</button>
+    //   <button style={
+    //     {
+    //       color : "red"
+    //     }
+    //   }>Style</button>
+    // </div>
+    // <div>
+    //     { marks>80 ? <h1>Passed</h1> : <h2>Failed</h2> }
+    // </div>
+
+    // <div>
+    //   {(
+    //     ()=>{
+    //       if(marks > 90){
+    //         return <h1>Result is : A+</h1>
+    //       }
+    //       else if(marks >=80 && marks <90){
+    //         return <h1>Result is : A</h1>
+    //       }
+    //       else{
+    //         return <h1>Failed</h1>
+    //       }
+    //     }
+    //   )()}
+    // </div>
+
+    // <div>
+    //   <ul>
+    //     {
+    //       city.map((cities,iterate)=>{
+    //         return <li key={iterate.toString()}>
+    //           {cities}
+    //         </li>
+    //       })
+    //     }
+    //   </ul>
+    // </div>
+
+    // <div>
+    //     <ol>
+    //       {
+    //         letter.map((alphabet,iterate)=>{
+    //           return <li key={iterate.toString()}>
+    //             {alphabet}
+    //           </li>
+    //         })
+    //       }
+    //     </ol>
+    // </div>
    
 
 
