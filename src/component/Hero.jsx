ef50c242a7feab87a0ffc67f9f87760b6e641ae6
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2025-09-09 18:00:09 +0600

    Update Hero.jsx

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,7 +1,11 @@
-const Hero = () =>{
+const Hero = (receiving_props) =>{
     return (
         <div>
-            <img src="https://alphacoders.com/lionel-messi-4k-wallpapers"></img>
+            {/* <img src="https://alphacoders.com/lionel-messi-4k-wallpapers"></img> */}
+            {/* <h1>{receiving_props.title}</h1>
+
+            <h2>{receiving_props.des}</h2> */}
+
         </div>
     );
 }
