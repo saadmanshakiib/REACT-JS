75a425364916a710263acffd3165aaf582f05f30
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2025-09-09 23:14:21 +0600

    Update Hero.jsx

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,10 +1,20 @@
-const Hero = (receiving_props) =>{
+const Hero = (props) =>{
     return (
         <div>
             {/* <img src="https://alphacoders.com/lionel-messi-4k-wallpapers"></img> */}
             {/* <h1>{receiving_props.title}</h1>
 
             <h2>{receiving_props.des}</h2> */}
+
+            {/* <ul>
+
+                <li>{props.item["name"]}</li>
+                <li>{props.item["age"]}</li>
+                <li>{props.item["city"]}</li>
+
+            </ul> */}
+
+           {/* <button onClick={props.btnclk}>Click For Error</button> */}
 
         </div>
     );
